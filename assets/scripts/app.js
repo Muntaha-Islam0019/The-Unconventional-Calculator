3750f539a8a8bb3ef38464352f0ea82ca870d9f9
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-04-22 23:18:54 +0600

    Played with backticks

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const defaultResult = 0
 let currentResult = defaultResult;
 currentResult = currentResult + 10 + 19 - 9 / 3;
 
-let calculatorDescription = '(' + defaultResult + ' + 10) + 19 - 9 / 3';
+// backtick gives ${} ability
+// this construct is called template literal
+// whitespace is allowed
+let calculatorDescription = `(${defaultResult} + 10) + 19 - 9 / 3`;
 
 outputResult(currentResult, calculatorDescription);
