a82617f40f795efdefde235746be9d1555b212cc
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-04-23 17:04:10 +0600

    Introduction to Functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
-const defaultResult = 0
+const defaultResult = 0;
 
 let currentResult = defaultResult;
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('The result is : ' + result);
+}
+
+add(19, 47);
+
 currentResult = currentResult + 10 + 19 - 9 / 3;
 
 // backtick gives ${} ability
